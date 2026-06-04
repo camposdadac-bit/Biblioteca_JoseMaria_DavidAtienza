b531e763215d4154eb4247f1a9739b94a2cc8bba
Chema <josealdaia2007@gmail.com>
2026-06-04 14:30:52 +0200

    mini retoques

--- biblioteca.py
+++ biblioteca.py
@@ -224,10 +224,7 @@ def list_usuarios():
 
 
 def habilita_usuario(id_usuario):
-    """Pone habilitado=False en la BD y en la lista en memoria.
-    Si no existe devuelve False, si falla por excepción guarda el error
-    devuelve False. Si existe en BD pero no en la lista el bucle no
-    encuentra nada y no rompe"""
+    """Revisa la existencia del usuario a través del id introducido. Para despues habilitarlo modificando su atributo de habilitado"""
     global ultimo_error
 
     try:
